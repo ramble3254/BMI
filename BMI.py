21f5e7fcbbb6d58d667858cd327a7180aab610c6
SmallK Lee <ramble3254@gmail.com>
2019-05-17 15:48:44 +0800

    add condition

--- BMI.py
+++ BMI.py
@@ -2,17 +2,20 @@ height = input('請輸入身高(cm): ')
 weight = input('請輸入體重(kg): ')
 height = float(height) / 100
 weight = float(weight)
-BMI = weight / (height * height)
-print('您的BMI值為: ',round(BMI,2))
-if BMI < 18.5:
-	print('過輕')
-elif BMI >= 18.5 and BMI <= 24:
-    print('正常')
-elif BMI >= 24 and BMI <= 27:
-	print('過重')
-elif BMI >= 27 and BMI <= 30:
-	print('輕度肥胖')
-elif BMI >= 30 and BMI <= 35:
-	print('中度肥胖')
+if height > 0 and weight > 0:
+	BMI = weight / (height * height)
+	print('您的BMI值為: ',round(BMI,2))
+	if BMI < 18.5:
+		print('過輕')
+	elif BMI >= 18.5 and BMI <= 24:
+		print('正常')
+	elif BMI >= 24 and BMI <= 27:
+		print('過重')
+	elif BMI >= 27 and BMI <= 30:
+		print('輕度肥胖')
+	elif BMI >= 30 and BMI <= 35:
+		print('中度肥胖')
+	else:
+		print('重度肥胖')
 else:
-	print('重度肥胖')
+	print("請輸入正確身高/體重")
